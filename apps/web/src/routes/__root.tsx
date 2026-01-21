88a6fa1a0a0a23691146df2fdc22578865fbdd68
Ryuichi <ryuichi.hayashi@gmail.com>
2026-01-21 15:58:08 +0900

    chore: アプリタイトルを「ネコGo!」に変更

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -4,9 +4,9 @@ import { createTanstackQueryUtils } from "@orpc/tanstack-query";
 import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
-	createRootRouteWithContext,
-	HeadContent,
-	Outlet,
+  createRootRouteWithContext,
+  HeadContent,
+  Outlet,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { useState } from "react";
@@ -17,42 +17,42 @@ import { link, type orpc } from "@/utils/orpc";
 import "../index.css";
 
 export interface RouterAppContext {
-	orpc: typeof orpc;
-	queryClient: QueryClient;
+  orpc: typeof orpc;
+  queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<RouterAppContext>()({
-	component: RootComponent,
-	head: () => ({
-		meta: [
-			{
-				title: "ネコパズル",
-			},
-			{
-				name: "description",
-				content: "ネコパズル - 監視カメラを避けてゴールを目指せ！",
-			},
-		],
-		links: [
-			{
-				rel: "icon",
-				href: "/favicon.ico",
-			},
-		],
-	}),
+  component: RootComponent,
+  head: () => ({
+    meta: [
+      {
+        title: "ネコGo!",
+      },
+      {
+        name: "description",
+        content: "ネコGo! - 監視カメラを避けてゴールを目指せ！",
+      },
+    ],
+    links: [
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+      },
+    ],
+  }),
 });
 
 function RootComponent() {
-	const [client] = useState<AppRouterClient>(() => createORPCClient(link));
-	const [_orpcUtils] = useState(() => createTanstackQueryUtils(client));
+  const [client] = useState<AppRouterClient>(() => createORPCClient(link));
+  const [_orpcUtils] = useState(() => createTanstackQueryUtils(client));
 
-	return (
-		<>
-			<HeadContent />
-			<Outlet />
-			<Toaster richColors />
-			<TanStackRouterDevtools position="bottom-left" />
-			<ReactQueryDevtools position="bottom" buttonPosition="bottom-right" />
-		</>
-	);
+  return (
+    <>
+      <HeadContent />
+      <Outlet />
+      <Toaster richColors />
+      <TanStackRouterDevtools position="bottom-left" />
+      <ReactQueryDevtools position="bottom" buttonPosition="bottom-right" />
+    </>
+  );
 }
